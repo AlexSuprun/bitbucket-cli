0259880e2c43127e05a57a8fb7aec4a45b16e385
0pilatos0 <paulvanderlei@hotmail.nl>
2025-12-28 12:14:56 +0100

    chore: remove old command files replaced by command pattern

--- src/commands/auth/logout.ts
+++
@@ -1,9 +0,0 @@
-import { clearConfig } from "../../lib/config.js";
-import { clearApiClient } from "../../api/client.js";
-import { outputSuccess } from "../../lib/output.js";
-
-export async function logout(): Promise<void> {
-  await clearConfig();
-  clearApiClient();
-  outputSuccess("Logged out of Bitbucket");
-}
